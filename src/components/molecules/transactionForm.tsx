a20a245b923513b5d517b415103ec780ac8ff695
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2024-07-01 21:05:33 +0530

    propject not over

---
+++ src/components/molecules/transactionForm.tsx
@@ -0,0 +1,128 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form"
+import { Button } from "../ui/button"
+import { Input } from "../ui/input"
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group"
+import { collection, addDoc } from "firebase/firestore";
+
+import { db } from "../../lib/firebase"
+
+const formSchema = z.object({
+ title : z.string().min(2,{
+    message: "add minimum two character"
+ }),
+ description : z.string().optional(),
+ amount : z.string(),
+ transactionTypes : z.string(),
+})
+
+const TransactionForm = () => {
+  
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+     title : "",
+     amount : "",
+     description : "",
+     transactionTypes : "",
+    },
+  })
+
+   async function onSubmit(values: z.infer<typeof formSchema>) {
+       console.log(values)
+       alert("done")
+       const docRef = await addDoc(collection(db, "transactions"), {
+        title : values.title,
+        description : values.description,
+        amount : values.amount,
+        transactionTypes : values.transactionTypes
+      });
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input placeholder="" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Input placeholder="" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="amount"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Amount</FormLabel>
+              <FormControl>
+                <Input placeholder="" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="transactionTypes"
+          render={({ field }) => (
+            <FormItem className="space-y-3">
+              <FormLabel>Choose Transaction</FormLabel>
+              <FormControl>
+                <RadioGroup
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  className="flex flex-col space-y-1"
+                >
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="all" />
+                    </FormControl>
+                    <FormLabel className="font-normal">
+                     Expance
+                    </FormLabel>
+                  </FormItem>
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="mentions" />
+                    </FormControl>
+                    <FormLabel className="font-normal">
+                      Income
+                    </FormLabel>
+                  </FormItem>
+                </RadioGroup>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full">Submit</Button>
+      </form>
+    </Form>
+  )
+}
+
+export default TransactionForm
--- src/lib/pages/home.tsx
+++ src/lib/pages/home.tsx
@@ -1,8 +1,26 @@
+import TransactionForm from "../../components/molecules/transactionForm"
+import { Button } from "../../components/ui/button"
+import { Dialog, DialogTrigger, DialogContent, DialogTitle, DialogDescription } from "../../components/ui/dialog"
+import { DialogHeader } from "../../components/ui/dialog"
 
 
 const home = () => {
   return (
-    <div>home</div>
+    <>
+    <h1>Expance</h1>
+    <Dialog>
+    <DialogTrigger><Button>Add Transaction</Button></DialogTrigger>
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Add Expances</DialogTitle>
+        <DialogDescription>
+          Manage your transition and update your balance
+        </DialogDescription>
+      </DialogHeader>
+      <TransactionForm/>
+    </DialogContent>
+  </Dialog>
+  </>
   )
 }
 
